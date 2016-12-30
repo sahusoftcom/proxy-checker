38ae004ed8cf971c9a71b9b99701dfe8c0df47cd
faizahmedfarooqui <faiz@sahusoft.com>
2016-12-30 12:20:38 +0530

    Initiate

---
+++ src/ProxyCheckerServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SahusoftCom\ProxyChecker;
+use Illuminate\Support\ServiceProvider;
+
+class ProxyCheckerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
